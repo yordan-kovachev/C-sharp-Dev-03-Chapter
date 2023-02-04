eaad90400753658b52c2f62c4bc678a4f69dea3d
unknown <yordan.kovachev@gmail.com>
2023-02-04 11:35:02 +0000

    03 Chapter initial repo commit

---
+++ 01 Task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишете израз, който да проверява дали дадено цяло число е четно
+//или нечетно.
+
+namespace _01_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, reminder;
+            Console.WriteLine("Please enter a interger value: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            reminder = number % 2;
+            if (reminder == 0)
+            {
+                Console.WriteLine("{0} is an even number", number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd number", number);
+            }
+        }
+    }
+}
